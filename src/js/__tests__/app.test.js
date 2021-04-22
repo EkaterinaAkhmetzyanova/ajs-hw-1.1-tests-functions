eeebdbb657d865defb92050c1fbcca8f0331736a
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-04-22 15:39:37 +0300

    eslint fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,25 +1,24 @@
-import getHealthStatus from "../app";
+import getHealthStatus from '../app';
 
-test("getHealthStatus = healthy", () => {
-    const result = getHealthStatus({ name: "Lala", health: 100 });
-    expect(result).toBe("healthy");
+test('getHealthStatus = healthy', () => {
+  const result = getHealthStatus({ name: 'Lala', health: 100 });
+  expect(result).toBe('healthy');
 });
 
-test("getHealthStatus = wounded", () => {
-    const result = getHealthStatus({ name: "Bebe", health: 23 });
-    expect(result).toBe("wounded");
+test('getHealthStatus = wounded', () => {
+  const result = getHealthStatus({ name: 'Bebe', health: 23 });
+  expect(result).toBe('wounded');
 });
 
-test("getHealthStatus = critical", () => {
-    const result = getHealthStatus({ name: "Boo", health: 7 });
-    expect(result).toBe("critical");
+test('getHealthStatus = critical', () => {
+  const result = getHealthStatus({ name: 'Boo', health: 7 });
+  expect(result).toBe('critical');
 });
 
-test("getHealthStatus", () => {
-    const result = getHealthStatus({ name: "Mimi" });
-    expect(result).toBe(0);
+test('getHealthStatus', () => {
+  const result = getHealthStatus({ name: 'Mimi' });
+  expect(result).toBe(0);
 });
-
 
 // test("getHealthStatus", () => {
 //     const result = getHealthStatus({ name: "Lala", health: 100 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 export default function getHealthStatus({ name, health }) {
-    let healthStatus = "healthy";
-    if (health <= 50 && health >= 15) {
-        healthStatus = "wounded";
-    } else if (health < 15) {
-        healthStatus = "critical";
-    } else if (health === undefined || health === null) {
-        return 0;
-    }
-    return healthStatus;
+  let healthStatus = 'healthy';
+  if (health <= 50 && health >= 15) {
+    healthStatus = 'wounded';
+  } else if (health < 15) {
+    healthStatus = 'critical';
+  } else if (health === undefined || health === null) {
+    return 0;
+  }
+  return healthStatus;
 }
 
-const userHealthStatus = getHealthStatus({ name: "Маг", health: 90 });
+const userHealthStatus = getHealthStatus({ name: 'Маг', health: 90 });
